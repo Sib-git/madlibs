8df08337b1bff1e1e48eeef6222024d83d0d1f91
SIBSANKAR RANA <isibsankarrana@gmail.com>
2023-03-19 12:30:27 +0530

    madlibs
    This is just string concatetation

---
+++ madlibs.py
@@ -0,0 +1,19 @@
+# Madlibs using string concatination
+
+# string concatination 
+# yotubeber="sibsankar rana "
+
+# print("subscribe to "+yotubeber)
+# print("subscribe to {}".format(yotubeber))
+# print(f"subscribe to {yotubeber}")
+
+adj=input("Adjective: ")
+verb1= input("Verb: ")
+verb2= input("Verb: ")
+famous_person = input("Famous_person: ")
+
+
+madlib = f"Coumputer programing is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
